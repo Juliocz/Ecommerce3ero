5c9da8e1a5f9990375072aecbcb58ced0d9bbbc2
JulioGisul <julio.llampa@gisul.com.bo>
2022-06-04 16:12:42 -0400

    sqldate

--- jshop.sql
+++ jshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2022 a las 00:19:12
+-- Tiempo de generación: 04-06-2022 a las 22:12:21
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.14
 
@@ -30,19 +30,27 @@ SET time_zone = "+00:00";
 CREATE TABLE `user` (
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
-  `celular` int(20) NOT NULL,
+  `celular` int(20) DEFAULT NULL,
   `correo` varchar(200) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `estado` varchar(30) NOT NULL,
   `tipo_usuario` varchar(100) NOT NULL,
-  `departamento` varchar(50) NOT NULL,
-  `ubicacion_lat` varchar(50) NOT NULL,
-  `ubicacion_lon` varchar(50) NOT NULL,
+  `departamento` varchar(50) DEFAULT NULL,
+  `ubicacion_lat` varchar(50) DEFAULT NULL,
+  `ubicacion_lon` varchar(50) DEFAULT NULL,
   `fecha_registro` datetime NOT NULL,
-  `fecha_mod` datetime NOT NULL,
+  `fecha_mod` datetime DEFAULT NULL,
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`nombre`, `apellido`, `celular`, `correo`, `usuario`, `password`, `estado`, `tipo_usuario`, `departamento`, `ubicacion_lat`, `ubicacion_lon`, `fecha_registro`, `fecha_mod`, `id`) VALUES
+('Julio', 'Llampa', NULL, 'julio@fsdf.com', 'julio..ssdf', 'e10adc3949ba59abbe56e057f20f883e', 'pendiente', 'user_simple', NULL, NULL, NULL, '2022-06-04 19:37:59', NULL, 1),
+('Julio C.', 'LLG', 69212155, 'jllampa@incos-santacruz.edu.bo', 'AdminJulio123', '32dc4eb266b2f639c7abf59fd957e2ca', 'activo', 'super_admin', 'SCZ', NULL, NULL, '2022-06-04 19:43:18', NULL, 2);
 
 --
 -- Índices para tablas volcadas
@@ -63,7 +71,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
